89cdca73fbfd5955947ff3adfaabb730ce938413
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-04-08 01:52:40 +0200

    modif models

--- models/Products.php
+++ models/Products.php
@@ -12,12 +12,12 @@ class Products
     public $image;
     public $id_categorie;
     public $date_creation;
+    public $keywords;
 
     public function __construct($pdo)
     {
         $this->pdo = $pdo;
     }
-
     public function read()
     {
         $query = 'SELECT * FROM ' . $this->table;
@@ -27,92 +27,90 @@ class Products
         extract($result);
         return $result;
     }
-
-    public function read_single($id)
+    public function read_single()
     {
-        $query = 'SELECT * FROM ' . $this->table . ' WHERE id_produit = ?';
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE id_produit = :id_produit';
         $stmt = $this->pdo->prepare($query);
-        $stmt->bindParam(1, $id);
+        $stmt -> bindParam(':id_produit', $this->id_produit);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         extract($result);
         return $result;
-    }
-    public function create()
+    }public function create()
     {
         $query = 'INSERT INTO ' . $this->table . ' SET nom = :nom, description = :description, prix = :prix, image = :image, id_categorie = :id_categorie';
+        $nom = htmlspecialchars(strip_tags($this->nom));
+        $description = htmlspecialchars(strip_tags($this->description));
+        $prix = htmlspecialchars(strip_tags($this->prix));
+        $image = htmlspecialchars(strip_tags($this->image));
+        $id_categorie = htmlspecialchars(strip_tags($this->id_categorie));
         $stmt = $this->pdo->prepare($query);
-        $this->nom = htmlspecialchars(strip_tags($this->nom));
-        $this->description = htmlspecialchars(strip_tags($this->description));
-        $this->prix = htmlspecialchars(strip_tags($this->prix));
-        $this->image = htmlspecialchars(strip_tags($this->image));
-        $this->id_categorie = htmlspecialchars(strip_tags($this->id_categorie));
-        $stmt->bindParam(':nom', $this->nom);
-        $stmt->bindParam(':description', $this->description);
-        $stmt->bindParam(':prix', $this->prix);
-        $stmt->bindParam(':image', $this->image);
-        $stmt->bindParam(':id_categorie', $this->id_categorie);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':prix', $prix);
+        $stmt->bindParam(':image', $image);
+        $stmt->bindParam(':id_categorie', $id_categorie);
         if ($stmt->execute()) {
             return true;
         }
         printf("Error: %s.\n", $stmt->error);
         return false;
     }
-    //public function update()
-    //{
-    //    $query = 'UPDATE ' . $this->table . ' SET name = :name, description = :description, price = :price, image = :image, category_id = :category_id WHERE id = :id';
-    //    $stmt = $this->pdo->prepare($query);
-    //    $this->name = htmlspecialchars(strip_tags($this->name));
-    //    $this->description = htmlspecialchars(strip_tags($this->description));
-    //    $this->price = htmlspecialchars(strip_tags($this->price));
-    //    $this->image = htmlspecialchars(strip_tags($this->image));
-    //    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
-    //    $this->id = htmlspecialchars(strip_tags($this->id));
-    //    $stmt->bindParam(':name', $this->name);
-    //    $stmt->bindParam(':description', $this->description);
-    //    $stmt->bindParam(':price', $this->price);
-    //    $stmt->bindParam(':image', $this->image);
-    //    $stmt->bindParam(':category_id', $this->category_id);
-    //    $stmt->bindParam(':id', $this->id);
-    //    if ($stmt->execute()) {
-    //        return true;
-    //    }
-    //    printf("Error: %s.\n", $stmt->error);
-    //    return false;
-    //}
-    //public function create()
-    //{
-    //    $query = 'INSERT INTO ' . $this->table . ' SET name = :name, description = :description, price = :price, image = :image, category_id = :category_id';
-    //    $stmt = $this->pdo->prepare($query);
-    //    $this->name = htmlspecialchars(strip_tags($this->name));
-    //    $this->description = htmlspecialchars(strip_tags($this->description));
-    //    $this->price = htmlspecialchars(strip_tags($this->price));
-    //    $this->image = htmlspecialchars(strip_tags($this->image));
-    //    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
-    //    $stmt->bindParam(':name', $this->name);
-    //    $stmt->bindParam(':description', $this->description);
-    //    $stmt->bindParam(':price', $this->price);
-    //    $stmt->bindParam(':image', $this->image);
-    //    $stmt->bindParam(':category_id', $this->category_id);
-    //    if ($stmt->execute()) {
-    //        return true;
-    //    }
-    //    printf("Error: %s.\n", $stmt->error);
-    //    return false;
-    //}
-//
-    //public function delete()
-    //{
-    //    $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
-    //    $stmt = $this->pdo->prepare($query);
-    //    $this->id = htmlspecialchars(strip_tags($this->id));
-    //    $stmt->bindParam(':id', $this->id);
-    //    if ($stmt->execute()) {
-    //        return true;
-    //    }
-    //    printf("Error: %s.\n", $stmt->error);
-    //    return false;
-    //}
+    public function update()
+    {
+        $query = 'UPDATE ' . $this->table . ' SET nom = :nom, description = :description, prix = :prix, image = :image, id_categorie = :id_categorie WHERE id_produit = :id_produit';
+        $nom = htmlspecialchars(strip_tags($this->nom));
+        $description = htmlspecialchars(strip_tags($this->description));
+        $prix = htmlspecialchars(strip_tags($this->prix));
+        $image = htmlspecialchars(strip_tags($this->image));
+        $id_categorie = htmlspecialchars(strip_tags($this->id_categorie));
+        $id = htmlspecialchars(strip_tags($this->id_produit));
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':prix', $prix);
+        $stmt->bindParam(':image', $image);
+        $stmt->bindParam(':id_categorie', $id_categorie);
+        $stmt->bindParam(':id_produit', $id);
+        if ($stmt->execute()) {
+            return true;
+        }
+        printf("Error: %s.\n", $stmt->error);
+        return false;
+    }
+    public function delete()
+    {
+        $query = 'DELETE FROM ' . $this->table . ' WHERE id_produit = :id_produit';
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':id_produit', $this->id_produit);
+        if ($stmt->execute()) {
+            return true;
+        }
+        printf("Error: %s.\n", $stmt->error);
+        return false;
+    }
+    public function search()
+    {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE nom LIKE ? OR description LIKE ?';
+        $stmt = $this->pdo->prepare($query);
+        $keywords = htmlspecialchars(strip_tags($this->keywords));
+        $keywords = "%{$keywords}%";
+        $stmt->bindParam(1, $keywords);
+        $stmt->bindParam(2, $keywords);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        extract($result);
+        return $result;
+    }
+    public function count()
+    {
+        $query = 'SELECT COUNT(*) as total_ligne FROM ' . $this->table;
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        extract($result);
+        return $result;
+    }
 //
     //public function search($keywords)
     //{
@@ -153,13 +151,6 @@ class Products
     //    $stmt->execute();
     //    return $stmt;
     //}
-    public function count()
-    {
-        $query = 'SELECT COUNT(*) as total_ligne FROM ' . $this->table;
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $row['total_ligne'];
-    }
+
 }
 
--- models/Users.php
+++ models/Users.php
@@ -11,12 +11,12 @@ class Users
     public $password;
     public $date_creation;
     public $id_role;
+    public $keywords;
 
     public function __construct($pdo)
     {
         $this->pdo = $pdo;
     }
-
     public function read()
     {
         $query = 'SELECT * FROM ' . $this->table;
@@ -26,49 +26,94 @@ class Users
         extract($result);        
         return $result;
     }
-
-    public function read_single($id)
+    public function read_single()
     {
-        $query = 'SELECT * FROM ' . $this->table . ' WHERE id_utilisateur = ?';
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE id_utilisateur = :id_utilisateur';
         $stmt = $this->pdo->prepare($query);
-        $stmt->bindParam(1, $id);
+        $stmt->bindParam(':id_utilisateur', $this->id_utilisateur);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         extract($result);
         return $result;
     }
-
-    public function check_user($email, $password)
+    public function connect()
     {
         $query = 'SELECT * FROM ' . $this->table . ' WHERE email = :email';
+        $email = htmlspecialchars(strip_tags($this->email));
+        $password = htmlspecialchars(strip_tags($this->password));
         $stmt = $this->pdo->prepare($query);
-        $stmt->execute(['email' => $email]);
-        $user = $stmt->fetch();
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($user && password_verify($password, $user['mot_de_passe'])) {
             return $user;
         }
         return false;
     }
-
-    public function register_user($nom, $prenom, $email, $password, $id_role)
+    public function register()
     {
         $query = 'INSERT INTO ' . $this->table . ' (nom, prenom, email, mot_de_passe, id_role) VALUES (:nom, :prenom, :email, :password, :id_role)';
+        $email = htmlspecialchars(strip_tags($this->email));
+        $password = htmlspecialchars(strip_tags($this->password));
+        $id_role = htmlspecialchars(strip_tags($this->id_role));
+        $nom = htmlspecialchars(strip_tags($this->nom));
+        $prenom = htmlspecialchars(strip_tags($this->prenom));
         $stmt = $this->pdo->prepare($query);
-        $stmt->execute(['nom' => $nom, 'prenom' => $prenom, 'email' => $email, 'password' => $password, 'id_role' => $id_role]);
+        $password = password_hash($password, PASSWORD_BCRYPT);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':id_role', $id_role);
+        $stmt->execute();
         return $stmt;
     }
-    public function update_user($nom, $prenom, $email, $id_role, $id_utilisateur)
+    public function update()
     {
         $query = 'UPDATE ' . $this->table . ' SET nom = :nom, prenom = :prenom, email = :email, id_role = :id_role WHERE id_utilisateur = :id_utilisateur';
+        $nom = htmlspecialchars(strip_tags($this->nom));
+        $prenom = htmlspecialchars(strip_tags($this->prenom));
+        $email = htmlspecialchars(strip_tags($this->email));
+        $id_role = htmlspecialchars(strip_tags($this->id_role));
+        $id_utilisateur = htmlspecialchars(strip_tags($this->id_utilisateur));
         $stmt = $this->pdo->prepare($query);
-        $stmt->execute(['nom' => $nom, 'prenom' => $prenom, 'email' => $email, 'id_role' => $id_role, 'id_utilisateur' => $id_utilisateur]);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':id_role', $id_role);
+        $stmt->bindParam(':id_utilisateur', $id_utilisateur);
+        $stmt->execute();
         return $stmt;
     }
-    public function delete_user($id_utilisateur)
+    public function delete()
     {
         $query = 'DELETE FROM ' . $this->table . ' WHERE id_utilisateur = :id_utilisateur';
         $stmt = $this->pdo->prepare($query);
-        $stmt->execute(['id_utilisateur' => $id_utilisateur]);
+        $stmt->bindParam(':id_utilisateur', $this->id_utilisateur);
+        $stmt->execute();
         return $stmt;
+    }
+    public function count()
+    {
+        $query = 'SELECT COUNT(*) as total_ligne FROM ' . $this->table;
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        extract($result);
+        return $result;
+    }
+    public function search()
+    {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE nom LIKE ? OR prenom LIKE ? OR email LIKE ?';
+        $stmt = $this->pdo->prepare($query);
+        $keywords = htmlspecialchars(strip_tags($this->keywords));
+        $keywords = "%{$keywords}%";
+        $stmt->bindParam(1, $keywords);
+        $stmt->bindParam(2, $keywords);
+        $stmt->bindParam(3, $keywords);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        extract($result);
+        return $result;
     }
 }
